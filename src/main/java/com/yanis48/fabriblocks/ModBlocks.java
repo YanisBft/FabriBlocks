a30b01c91cd51a76d09722c837c427be489f53e4
Yanis48 <doublecraft.official@gmail.com>
2019-01-15 15:53:32 +0100

    Changed hardness and resistance to match vanilla values

--- src/main/java/com/yanis48/fabriblocks/ModBlocks.java
+++ src/main/java/com/yanis48/fabriblocks/ModBlocks.java
@@ -32,22 +32,22 @@ public class ModBlocks {
 	public static FBBlock SCUTE_BLOCK;
 	
 	public static void init() {
-		SNOW_BRICKS = new FBBlock("snow_bricks", 2.0f, 30.0f);
-		SNOW_BRICK_STAIRS = new FBStairs(SNOW_BRICKS.getDefaultState(), "snow_brick_stairs", 2.0f, 30.0f);
-		SNOW_BRICK_SLAB = new FBSlab("snow_brick_slab", 2.0f, 30.0f);
-		SNOW_BRICK_WALL = new FBWall("snow_brick_wall", 2.0f, 30.0f);
-		STONE_WALL = new FBWall("stone_wall", 2.0f, 30.0f);
-		SMOOTH_STONE_WALL = new FBWall("smooth_stone_wall", 2.0f, 30.0f);
-		SMOOTH_SANDSTONE_WALL = new FBWall("smooth_sandstone_wall", 2.0f, 30.0f);
-		SMOOTH_RED_SANDSTONE_WALL = new FBWall("smooth_red_sandstone_wall", 2.0f, 30.0f);
-		POLISHED_GRANITE_WALL = new FBWall("polished_granite_wall", 2.0f, 30.0f);
-		POLISHED_DIORITE_WALL = new FBWall("polished_diorite_wall", 2.0f, 30.0f);
-		POLISHED_ANDESITE_WALL = new FBWall("polished_andesite_wall", 2.0f, 30.0f);
-		QUARTZ_WALL = new FBWall("quartz_wall", 2.0f, 30.0f);
-		PRISMARINE_BRICK_WALL = new FBWall("prismarine_brick_wall", 2.0f, 30.0f);
-		DARK_PRISMARINE_WALL = new FBWall("dark_prismarine_wall", 2.0f, 30.0f);
-		PURPUR_WALL = new FBWall("purpur_wall", 2.0f, 30.0f);
-		SCUTE_BLOCK = new FBBlock("scute_block", 2.0f, 30.0f);
+		SNOW_BRICKS = new FBBlock("snow_bricks", 1.5f, 30.0f);
+		SNOW_BRICK_STAIRS = new FBStairs(SNOW_BRICKS.getDefaultState(), "snow_brick_stairs", 1.5f, 30.0f);
+		SNOW_BRICK_SLAB = new FBSlab("snow_brick_slab", 1.5f, 30.0f);
+		SNOW_BRICK_WALL = new FBWall("snow_brick_wall", 1.5f, 30.0f);
+		STONE_WALL = new FBWall("stone_wall", 1.5f, 30.0f);
+		SMOOTH_STONE_WALL = new FBWall("smooth_stone_wall", 1.5f, 30.0f);
+		SMOOTH_SANDSTONE_WALL = new FBWall("smooth_sandstone_wall", 0.8f, 4.0f);
+		SMOOTH_RED_SANDSTONE_WALL = new FBWall("smooth_red_sandstone_wall", 0.8f, 4.0f);
+		POLISHED_GRANITE_WALL = new FBWall("polished_granite_wall", 1.5f, 30.0f);
+		POLISHED_DIORITE_WALL = new FBWall("polished_diorite_wall", 1.5f, 30.0f);
+		POLISHED_ANDESITE_WALL = new FBWall("polished_andesite_wall", 1.5f, 30.0f);
+		QUARTZ_WALL = new FBWall("quartz_wall", 0.8f, 4.0f);
+		PRISMARINE_BRICK_WALL = new FBWall("prismarine_brick_wall", 1.5f, 30.0f);
+		DARK_PRISMARINE_WALL = new FBWall("dark_prismarine_wall", 1.5f, 30.0f);
+		PURPUR_WALL = new FBWall("purpur_wall", 1.5f, 30.0f);
+		SCUTE_BLOCK = new FBBlock("scute_block", 1.0f, 5.0f);
 	}
 
 }

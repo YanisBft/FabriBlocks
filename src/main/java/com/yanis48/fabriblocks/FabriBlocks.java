fc63266920ba20b1d8d4eea3f0390774d5a14ae8
Yanis48 <doublecraft.official@gmail.com>
2019-01-16 23:45:33 +0100

    Updated to 19w03a!
    Updated mappings
    Updated version

--- src/main/java/com/yanis48/fabriblocks/FabriBlocks.java
+++ src/main/java/com/yanis48/fabriblocks/FabriBlocks.java
@@ -7,7 +7,7 @@ import net.fabricmc.api.ModInitializer;
 public class FabriBlocks implements ModInitializer {
 	
 	public static final String MOD_ID = "fabriblocks";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static final String NAME = "FabriBlocks";
 	public static Logger logger;
 	

16c2528e4543afb73cc97d0da3184c8544825f25
Yanis48 <doublecraft.official@gmail.com>
2019-01-28 21:42:00 +0100

    Fixed glass door playing wooden sounds when opened/closed
    Updated mappings too

--- src/main/java/com/yanis48/fabriblocks/block/FBDoor.java
+++ src/main/java/com/yanis48/fabriblocks/block/FBDoor.java
@@ -3,14 +3,20 @@ package com.yanis48.fabriblocks.block;
 import com.yanis48.fabriblocks.FabriBlocks;
 
 import net.fabricmc.fabric.block.FabricBlockSettings;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.Material;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.block.BlockItem;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 public class FBDoor extends DoorBlock {
 	
@@ -18,6 +24,14 @@ public class FBDoor extends DoorBlock {
 		super(FabricBlockSettings.of(Material.GLASS).sounds(BlockSoundGroup.GLASS).strength(hardness, resistance).build());
 		Registry.BLOCK.register(new Identifier(FabriBlocks.MOD_ID, name), this);
 		Registry.ITEM.register(new Identifier(FabriBlocks.MOD_ID, name), new BlockItem(this, new Item.Settings().stackSize(64).itemGroup(ItemGroup.REDSTONE)));
+	}
+	
+	@Override
+	public boolean activate(BlockState blockState, World world, BlockPos blockPos, PlayerEntity playerEntity, Hand hand, BlockHitResult blockHitResult) {
+		blockState = (BlockState)blockState.method_11572(OPEN);
+		world.setBlockState(blockPos, blockState, 10);
+		world.fireWorldEvent(playerEntity, (Boolean)blockState.get(OPEN) ? 1005 : 1011, blockPos, 0);
+		return true;
 	}
 
 }

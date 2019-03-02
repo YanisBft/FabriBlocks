5499548638b65f7de7f675c973b4bb4d2e1ec20a
Yanis48 <doublecraft.official@gmail.com>
2019-03-02 18:43:04 +0100

    Version 1.1.0!
    Removed useless static strings too

--- src/main/java/com/yanis48/fabriblocks/FabriBlocks.java
+++ src/main/java/com/yanis48/fabriblocks/FabriBlocks.java
@@ -8,8 +8,6 @@ import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 public class FabriBlocks implements ModInitializer {
 	
 	public static final String MOD_ID = "fabriblocks";
-	public static final String VERSION = "1.0.3";
-	public static final String NAME = "FabriBlocks";
 	
 	@Override
 	public void onInitialize() {

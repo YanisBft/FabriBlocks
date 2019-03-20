516768f996d02afd34a28466cc85928d9e5eccaf
Yanis48 <doublecraft.official@gmail.com>
2019-03-20 20:24:20 +0100

    Made leaf carpets and bookshelves flammable
    Updated to 19w12a too

--- src/main/java/com/yanis48/fabriblocks/FabriBlocks.java
+++ src/main/java/com/yanis48/fabriblocks/FabriBlocks.java
@@ -1,10 +1,12 @@
 package com.yanis48.fabriblocks;
 
+import com.yanis48.fabriblocks.init.ModBlockTags;
 import com.yanis48.fabriblocks.init.ModBlocks;
 import com.yanis48.fabriblocks.init.ModItemTags;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 
 public class FabriBlocks implements ModInitializer {
@@ -20,6 +22,9 @@ public class FabriBlocks implements ModInitializer {
 		FuelRegistry.INSTANCE.add(ModBlocks.BAMBOO_BLOCK, 300);
 		FuelRegistry.INSTANCE.add(ModBlocks.BAMBOO_TRAPDOOR, 300);
 		FuelRegistry.INSTANCE.add(ModBlocks.BAMBOO_DOOR, 200);
+		
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlockTags.LEAF_CARPETS, 60, 100);
+		FlammableBlockRegistry.getDefaultInstance().add(ModBlockTags.BOOKSHELVES, 30, 20);
 		
 		CompostingChanceRegistry.INSTANCE.add(ModBlocks.OAK_LEAF_CARPET, 0.1f);
 		CompostingChanceRegistry.INSTANCE.add(ModBlocks.SPRUCE_LEAF_CARPET, 0.1f);
--- src/main/java/com/yanis48/fabriblocks/init/ModBlockTags.java
+++ src/main/java/com/yanis48/fabriblocks/init/ModBlockTags.java
@@ -9,6 +9,8 @@ import net.minecraft.util.Identifier;
 
 public class ModBlockTags {
 	
+	public static final Tag<Block> LEAF_CARPETS = register("leaf_carpets");
+	public static final Tag<Block> BOOKSHELVES = register("bookshelves");
 	public static final Tag<Block> LECTERNS = register("lecterns");
 	
 	public static Tag<Block> register(String id) {

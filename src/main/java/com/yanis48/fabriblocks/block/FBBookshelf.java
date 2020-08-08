4e5dc70924cb42d5fa5472f68e68697d1c228ba7
Yanis48 <doublecraft.official@gmail.com>
2020-08-08 15:45:56 +0200

    fixed block classes

--- src/main/java/com/yanis48/fabriblocks/block/FBBookshelf.java
+++ src/main/java/com/yanis48/fabriblocks/block/FBBookshelf.java
@@ -2,7 +2,7 @@ package com.yanis48.fabriblocks.block;
 
 import com.yanis48.fabriblocks.FabriBlocks;
 
-import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.Item;
@@ -15,7 +15,7 @@ import net.minecraft.util.registry.Registry;
 public class FBBookshelf extends Block {
 	
 	public FBBookshelf(String name) {
-		super(FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(1.5f, 7.5f).build());
+		super(FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(1.5f, 7.5f));
 		Registry.register(Registry.BLOCK, new Identifier(FabriBlocks.MOD_ID, name), this);
 		Registry.register(Registry.ITEM, new Identifier(FabriBlocks.MOD_ID, name), new BlockItem(this, new Item.Settings().maxCount(64).group(ItemGroup.BUILDING_BLOCKS)));
 	}
--- src/main/java/com/yanis48/fabriblocks/block/FBStairs.java
+++ src/main/java/com/yanis48/fabriblocks/block/FBStairs.java
@@ -2,7 +2,7 @@ package com.yanis48.fabriblocks.block;
 
 import com.yanis48.fabriblocks.FabriBlocks;
 
-import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.StairsBlock;
@@ -15,7 +15,7 @@ import net.minecraft.util.registry.Registry;
 public class FBStairs extends StairsBlock {
 
 	public FBStairs(BlockState state, String name, float hardness, float resistance) {
-		super(state, FabricBlockSettings.of(Material.STONE).strength(hardness, resistance).build());
+		super(state, FabricBlockSettings.of(Material.STONE).strength(hardness, resistance));
 		Registry.register(Registry.BLOCK, new Identifier(FabriBlocks.MOD_ID, name), this);
 		Registry.register(Registry.ITEM, new Identifier(FabriBlocks.MOD_ID, name), new BlockItem(this, new Item.Settings().maxCount(64).group(ItemGroup.BUILDING_BLOCKS)));
 	}
